b889a2207f8afae8323962f867b58290e49efa04
layladani <112534575+layladani@users.noreply.github.com>
2022-09-02 23:56:35 -0400

    Add files via upload

---
+++ EvenOdd.java
@@ -0,0 +1,18 @@
+
+public class EvenOdd 
+{
+
+	public static void main(String[] args) 
+	{
+		int num = 16;
+		if (num%2 == 0)
+		{
+			System.out.println("number is Even");;
+		}
+		else
+		{
+			System.out.println("number is Odd");
+		}
+	}
+
+}
---
+++ StudentResult.java
@@ -0,0 +1,16 @@
+
+public class StudentResult {
+
+	public static void main(String[] args) {
+		int Sub1 = 75;
+		int Sub2 = 82;
+		int Sub3 = 78;
+		
+		int Sum = (Sub1+Sub2+Sub3);
+		System.out.println("your total marks is="+Sum);
+		System.out.println("your average is="+Sum/3);
+		System.out.println("your percentage is="+(Sum*100)/300+"%");
+
+	}
+
+}
